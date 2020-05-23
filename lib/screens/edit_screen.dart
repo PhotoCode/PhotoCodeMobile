e8ea57a4e2d5d869dbafc28e83e3eb8df0a125bc
Leon Si <leonzalion@gmail.com>
2020-05-23 19:30:16 -0400

    added code editor

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -1,18 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:rich_code_editor/exports.dart';
 
-class EditScreen extends StatefulWidget {
-  @override
-  EditScreenState createState() => EditScreenState();
-}
+import '../dummy_syntax_highlighter.dart';
 
-class EditScreenState extends State<EditScreen> {
+class EditScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      keyboardType: TextInputType.multiline,
-      maxLines: null,
-      style: TextStyle(
-        color: Colors.white,
+    return DemoCodeEditor();
+  }
+}
+
+class DemoCodeEditor extends StatefulWidget {
+  @override
+  _DemoCodeEditorState createState() => _DemoCodeEditorState();
+}
+
+class _DemoCodeEditorState extends State<DemoCodeEditor> {
+  RichCodeEditingController _rec;
+  SyntaxHighlighterBase _syntaxHighlighterBase;
+
+  @override
+  void initState() {
+    super.initState();
+    _syntaxHighlighterBase = DummySyntaxHighlighter();
+    _rec = RichCodeEditingController(_syntaxHighlighterBase);
+  }
+
+  @override
+  void dispose() {
+    //_richTextFieldState.currentState?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Dummy Editor"),
+      ),
+      body: Container(
+        height: 300.0,
+        margin: EdgeInsets.all(24.0),
+        padding: EdgeInsets.all(24.0),
+        decoration:
+            new BoxDecoration(border: new Border.all(color: Colors.grey)),
+        child: RichCodeField(
+          autofocus: true,
+          controller: _rec,
+          textCapitalization: TextCapitalization.none,
+          decoration: null,
+          syntaxHighlighter: _syntaxHighlighterBase,
+          maxLines: null,
+          onChanged: (String s) {},
+          onBackSpacePress: (TextEditingValue oldValue) {},
+          onEnterPress: (TextEditingValue oldValue) {
+            var result = _syntaxHighlighterBase.onEnterPress(oldValue);
+            if (result != null) {
+              _rec.value = result;
+            }
+          },
+        ),
       ),
     );
   }
