ee29f0d6d39c2fff7f340217883ac11ea4d31320
Leon Si <leonzalion@gmail.com>
2020-05-23 17:16:08 -0400

    working on snippets

--- lib/screens/snippets_screen.dart
+++ lib/screens/snippets_screen.dart
@@ -1,9 +1,18 @@
+import 'package:floating_search_bar/floating_search_bar.dart';
 import 'package:flutter/material.dart';
 
 class SnippetsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('Dragon is a noob.');
+    return FloatingSearchBar.builder(
+      itemCount: 2,
+      itemBuilder: (BuildContext context, int index) {
+        return _buildSnippet(index);
+      },
+      decoration: InputDecoration.collapsed(
+        hintText: 'Search',
+      ),
+    );
   }
 
   Widget _buildSnippet(int index) {
