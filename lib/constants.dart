06c8a7fbe1de9f066e6fa806c486c9b9d83f9a1b
Leon Si <leonzalion@gmail.com>
2020-05-23 13:41:22 -0400

    changed app name

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const String appName = 'ECORP';
-  static const String logoTag = 'near.huscarl.loginsample.logo';
-  static const String titleTag = 'near.huscarl.loginsample.title';
+  static const String appName = 'PhotoCode';
+  static const String logoTag = 'com.photocode.photocodemobile.logo';
+  static const String titleTag = 'com.photocode.photocodemobile.title';
 }
