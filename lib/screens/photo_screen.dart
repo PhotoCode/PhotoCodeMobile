0449e5cf64599993b757bda3dda908cd2a4b0962
AbstractUltra <45088170+abstractultra@users.noreply.github.com>
2020-05-24 03:19:54 -0400

    changes

--- lib/screens/photo_screen.dart
+++ lib/screens/photo_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:getflutter/getflutter.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:mobile/constants.dart';
 import 'package:mobile/screens/edit_screen.dart';
 import 'package:platform_action_sheet/platform_action_sheet.dart';
 import 'dart:async';
@@ -16,6 +17,8 @@ class PhotoScreen extends StatefulWidget {
 class _PhotoScreenState extends State<PhotoScreen> {
   File _image;
   String _ocrResult;
+
+  String _selectedLanguage = "Javascript";
 
   Future getImage(ImageSource source, BuildContext context) async {
     Dio dio = new Dio();
@@ -55,25 +58,68 @@ class _PhotoScreenState extends State<PhotoScreen> {
     );
   }
 
+  void changeLanguage(String newSelection) {
+    if (mounted) {
+      setState(() {
+        _selectedLanguage = newSelection;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
+        height: double.infinity,
+        width: double.infinity,
+        margin: EdgeInsets.fromLTRB(40, 0, 40, 0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            _image == null ? (
-              Text('No image selected.')
-            ) : (
-              Image.file(_image, height: 200)
+            Spacer(flex: 4),
+            Container(
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(40)),
+              ),
+              child: _image != null ? Image.file(_image, fit: BoxFit.contain) : null,
             ),
+            Spacer(),
             GFButton(
               onPressed: openSheet,
-              text: "Get Image"
+              fullWidthButton: true,
+              shape: GFButtonShape.pills,
+              color: Constants.barBackgroundColor,
+              size: GFSize.LARGE,
+              child: Text("Select Image", style: TextStyle(color: Constants.accentColor)),
             ),
+            Spacer(),
             GFButton(
               onPressed: openEditor, 
-              text: "Confirm",
+              fullWidthButton: true,
+              shape: GFButtonShape.pills,
+              color: Constants.barBackgroundColor,
+              size: GFSize.LARGE,
+              child: Text("Process Image", style: TextStyle(color: Constants.accentColor)),
             ),
+            Spacer(),
+            DropdownButton<String>(
+              value: _selectedLanguage,
+              dropdownColor: Constants.barBackgroundColor,
+              items: <String>['Javascript', 'More coming soon...'].map((String value) {
+                return new DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value, style: TextStyle(color: Constants.accentColor)),
+                );
+              }).toList(),
+              onChanged: (newSelection) {
+                changeLanguage(newSelection); 
+              }
+            ),
+            Spacer(flex: 4),
           ],
         ),
       ),
