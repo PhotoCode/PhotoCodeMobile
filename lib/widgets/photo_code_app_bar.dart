7dde08ca5ace7ea9c7313c2550d3409f4cd873a8
Leon Si <leonzalion@gmail.com>
2020-05-23 22:45:54 -0400

    hi

--- lib/widgets/photo_code_app_bar.dart
+++ lib/widgets/photo_code_app_bar.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/theme.dart';
 import 'package:flutter_login/widgets.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../constants.dart';
-import 'fade_in.dart';
 
 class PhotoCodeAppBar extends StatelessWidget with PreferredSizeWidget {
   static const headerAniInterval =
@@ -18,47 +16,33 @@ class PhotoCodeAppBar extends StatelessWidget with PreferredSizeWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    final menuBtn = IconButton(
-      color: theme.accentColor,
-      icon: const Icon(FontAwesomeIcons.bars),
-      onPressed: () {},
-    );
-    final title = Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Hero(
-              tag: Constants.logoTag,
-              child: Image.asset(
-                'assets/images/photocode.png',
-                filterQuality: FilterQuality.high,
-                height: 30,
-              ),
-            ),
-          ),
-          HeroText(
-            Constants.appName,
-            tag: Constants.titleTag,
-            viewState: ViewState.shrunk,
-            style: LoginThemeHelper.loginTextStyle,
-            smallFontSize: 30,
-          ),
-          SizedBox(width: 20),
-        ],
-      ),
-    );
-
     return AppBar(
-      leading: FadeIn(
-        child: menuBtn,
-        controller: this.loadingController,
-        offset: .3,
-        curve: headerAniInterval,
-        fadeDirection: FadeDirection.startToEnd,
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: Hero(
+          tag: Constants.logoTag,
+          child: Image.asset(
+            'assets/images/photocode.png',
+            filterQuality: FilterQuality.high,
+            height: 10,
+            width: 10,
+          ),
+        ),
       ),
-      title: title,
+      actions: [
+        Padding(
+          padding: EdgeInsets.only(right: 10),
+          child: Icon(Icons.more_horiz, color: Constants.accentColor, size: 30),
+        ),
+      ],
+      title: HeroText(
+        Constants.appName,
+        tag: Constants.titleTag,
+        viewState: ViewState.shrunk,
+        style: LoginThemeHelper.loginTextStyle,
+        smallFontSize: 30,
+      ),
+      centerTitle: true,
       backgroundColor: Constants.barBackgroundColor,
       elevation: 0,
       textTheme: theme.accentTextTheme,
