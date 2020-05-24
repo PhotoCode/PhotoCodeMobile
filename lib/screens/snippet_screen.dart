b30fa854fb73785b5342373feb1b59eadb05fede
AbstractUltra <45088170+abstractultra@users.noreply.github.com>
2020-05-24 05:22:56 -0400

    changes

--- lib/screens/snippet_screen.dart
+++ lib/screens/snippet_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_highlight/flutter_highlight.dart';
-import 'package:flutter_highlight/themes/github.dart';
+import 'package:flutter_highlight/theme_map.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:getflutter/getflutter.dart';
+import 'package:mobile/constants.dart';
 
 import '../widgets/photo_code_app_bar.dart';
 
@@ -18,15 +19,29 @@ class SnippetScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var theme = new Map<String, TextStyle>.from(themeMap['solarized-dark']);
+    theme["root"] = TextStyle(backgroundColor: Constants.backgroundColor, color: Constants.textColor);
     return Scaffold(
       appBar: PhotoCodeAppBar(showBackButton: true),
+      backgroundColor: Constants.backgroundColor,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Spacer(flex: 7),
+          Text(
+            name,
+            style: TextStyle(
+              color: Constants.textColor, 
+              fontSize: 25,
+            ),
+          ),
+          Spacer(flex: 4),
           Image(image: image),
           HighlightView(
             code,
             language: 'javascript',
-            theme: githubTheme,
+            theme: theme,
             padding: EdgeInsets.all(12),
             textStyle: TextStyle(
               fontFamily: 'RobotoMono',
@@ -34,21 +49,26 @@ class SnippetScreen extends StatelessWidget {
             ),
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Spacer(flex: 3),
               GFButton(
                 onPressed: () {},
                 color: GFColors.PRIMARY,
                 text: "Edit",
                 icon: Icon(FontAwesomeIcons.pencilAlt),
               ),
+              Spacer(),
               GFButton(
                 onPressed: () {},
                 color: GFColors.SUCCESS,
                 text: "Execute",
                 icon: Icon(FontAwesomeIcons.code),
               ),
+              Spacer(flex: 3),
             ],
           ),
+          Spacer(flex: 7),
         ],
       ),
     );
